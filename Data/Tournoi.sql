447574739a0292def0cc83e573451786876e4a0f
Irene Lefevre <irene.lefevre@orisonm.fr>
2022-11-28 13:26:01 +0100

    Correction

--- Data/Tournoi.sql
+++ Data/Tournoi.sql
@@ -1,9 +1,9 @@
-INSERT INTO tournoi (id, date, type,descrption,etat,nbParticipants)
-VALUES (1, '2023-01-24', 'Valorant Open Tour France', 'Le VALORANT OPEN TOUR FRANCE est le circuit compétitif français amateur sur VALORANT','Planifié','25'),
-       (2, '2012-09-20', 'Ultr_Team-League of Legends', 'Tournoi LOL','Terminé','250'),
-       (3, '2018-09-24', 'Overwatch Ligue', 'Tournoi Oberwatch US','Terminé','50'),
-       (4, '2022-12-24', 'DOJO', 'Tournoi Tekken','Planifié','25'),
-       (5, '2022-09-05', 'AWC', 'Tournoi World of Warcraft','En Cours','25'),
-       (6, '2023-01-13', 'MINISH 4.7', 'Tournoi Smash  Bros Ultimate','Planifié','25'),
-       (7, '2018-09-24', 'Nett Killing', 'Tournoi Animal Crossing','Terminé','25');
+INSERT INTO tournoi (id, date, type,descrption,etat)
+VALUES (1, '2023-01-24', 'Valorant Open Tour France', 'Le VALORANT OPEN TOUR FRANCE est le circuit compétitif français amateur sur VALORANT','Planifié'),
+       (2, '2012-09-20', 'Ultr_Team-League of Legends', 'Tournoi LOL','Terminé'),
+       (3, '2018-09-24', 'Overwatch Ligue', 'Tournoi Oberwatch US','Terminé'),
+       (4, '2022-12-24', 'DOJO', 'Tournoi Tekken','Planifié'),
+       (5, '2022-09-05', 'AWC', 'Tournoi World of Warcraft','En Cours'),
+       (6, '2023-01-13', 'MINISH 4.7', 'Tournoi Smash  Bros Ultimate','Planifié'),
+       (7, '2018-09-24', 'Nett Killing', 'Tournoi Animal Crossing','Terminé');
 
--- src/main/java/ila/project/tournament_manager/model/Tournoi.java
+++ src/main/java/ila/project/tournament_manager/model/Tournoi.java
@@ -3,6 +3,7 @@ package ila.project.tournament_manager.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.sql.Date;
 
