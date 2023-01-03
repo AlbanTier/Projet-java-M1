e5e3163b50fba4d690bdf3306e680717bcf5e40b
IreneLefevre <irene.lefevre.pro@gmail.com>
2023-01-03 09:37:47 +0100

    reparation du mapper

--- src/main/java/ila/api/mapper/TournoiMapper.java
+++ src/main/java/ila/api/mapper/TournoiMapper.java
@@ -1,8 +1,5 @@
 package ila.api.mapper;
 
-import org.mapstruct.Mapper;
-import org.springframework.data.domain.Page;
-
 
 
 import ila.api.dto.TournoiCreateDto;
@@ -11,15 +8,15 @@ import ila.api.dto.PageDto;
 import ila.project.tournament_manager.model.Tournoi;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
+import org.springframework.data.domain.Page;
 
 //@Mapper(componentModel = "spring", uses = {EquipeMapper.class, PageMapper.class})
-@Mapper(componentModel = "spring", uses = {PageMapper.class})
+@Mapper(componentModel = "spring", uses = {PageMapper.class, PageMapper.class})
 public interface TournoiMapper {
     Tournoi mapToEntity(TournoiCreateDto TournoiCreateDto);
     TournoiDto mapToDto(Tournoi Tournoi);
 
     @Mapping(target = "data", source = "content")
     @Mapping(target = "context", source = ".")
-    PageDto<TournoiDto> mapToPageDto(Page<Tournoi> page); 
+    PageDto<TournoiDto> mapToPageDto(Page<Tournoi> page);
 }
