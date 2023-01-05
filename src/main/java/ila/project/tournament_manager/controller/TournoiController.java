06544c25684656bc12cdbe22cdd7836520293593
test <landon.louise@gmail.com>
2023-01-05 10:58:37 +0100

    move folder

--- src/main/java/ila/project/tournament_manager/controller/TournoiController.java
+++ src/main/java/ila/project/tournament_manager/controller/TournoiController.java
@@ -1,4 +1,4 @@
-package ila.api;
+package ila.project.tournament_manager.controller;
 
 import org.springframework.web.bind.annotation.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
