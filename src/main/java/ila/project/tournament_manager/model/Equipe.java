a8d321ceb0976b81863fdfcf3a15f2d28580b6cb
AlbanTier <albannils03@gmail.com>
2023-01-05 16:36:19 +0100

    wip

--- src/main/java/ila/project/tournament_manager/model/Equipe.java
+++ src/main/java/ila/project/tournament_manager/model/Equipe.java
@@ -19,7 +19,7 @@ public class Equipe {
     private Long id;
 
     @Column(nullable = false)
-    private String teamName;
+    private String name;
 
     @ManyToMany(mappedBy = "equipes")
     private List<Joueur> joueurs;
