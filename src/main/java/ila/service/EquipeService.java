b46307e9c8a60c10faf7588cb99230fc116e9195
test <landon.louise@gmail.com>
2023-01-05 09:31:41 +0100

    Ajout des services Equipe et Joueurs

--- src/main/java/ila/service/EquipeService.java
+++ src/main/java/ila/service/EquipeService.java
@@ -1,2 +1,14 @@
-package ila.service;public interface EquipeService {
+package ila.service;
+
+import ila.project.tournament_manager.model.Equipe;
+import org.springframework.data.domain.Page;
+
+import java.util.Optional;
+
+public interface EquipeService {
+    Page<Equipe> getAllEquipes(String name, int page);
+
+    Optional<Equipe> getEquipeById(Long id);
+
+    Equipe saveEquipe(Equipe equipe);
 }
--- src/main/java/ila/service/JoueurService.java
+++ src/main/java/ila/service/JoueurService.java
@@ -1,2 +1,15 @@
-package ila.service;public interface JoueurService {
+package ila.service;
+
+import ila.project.tournament_manager.model.Joueur;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.Optional;
+
+public interface JoueurService {
+    Page<Joueur> getAllJoueurs(String name, PageRequest page);
+
+    Optional<Joueur> getJoueurById(Long id);
+
+    Joueur saveJoueur(Joueur joueur);
 }
